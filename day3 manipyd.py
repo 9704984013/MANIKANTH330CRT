b9239b341b4c356fdc4e06fd42888e6e10581543
9704984013 <162981101+9704984013@users.noreply.github.com>
2024-03-21 11:38:11 +0530

    Add files via upload

---
+++ day3 manipyd.py
@@ -0,0 +1,226 @@
+#length = int(input("enter the lenght of rectangula"))
+#breath = int(input("enter the breath of rectangular"))
+#if length==breath:
+#     print("its a square")
+#else: 
+ #   print("its not square")
+
+
+  
+# n = int(input("enter the number: "))
+#if n%5==0 and n%7==0:
+#    print("yes")
+#else:
+  #   print("no")
+
+#price = int(input("enter the amount"))
+#amount = 0
+#if price>=100000:
+  #   discount = price*(6/100)
+   #  value = price - discount
+  #   amount=value*(15/100)
+ #    print(value+amount)
+#else:
+ #   tax = price*(5/100)
+ #   total = price+tax
+ #   print("The on road cost of bike is: ",total)
+ 
+#if a>b and a>c:
+#     print("A is greater")
+#elif b>a and b>c:
+ #    print("B is greater")
+#elif c>a and c>b:
+ #    print("c is greater")
+
+# A school has following rules for grading system:
+# a. Below 25 - F
+# b. 25 to 45 - E
+# c. 45 to 50 - D
+# d. 50 to 60 - C
+# e. 60 to 80 - B
+# f. Above 80 - A
+# Ask user to enter marks and print the corresponding grade.
+
+#marks = int(input("enter the marks"))
+#if marks <25 :
+#    print("F")
+#else marks <45:
+#    print("E")
+#else marks <50:
+#   print("D")
+#else marks <60:
+ #   print("C")
+#else marks <80:
+ #   print("A")
+#elif:
+ #   print("Fail")
+
+
+# ! #9
+#b =# 60
+
+#If #a>b:
+#    #print("A")
+#ela#se:
+ #   #print("b")
+#  *#Rules
+#1.) #statement inside the if condition have to be present at first
+#2.) #elif cannot be used in short and if else
+#3.) #Always it have to end with else
+
+# print("A") if a>b else peint("B")
+
+#char = input("Enter the char: ")
+
+#if char =="a" or char =='e ' or char =='i' or char =='o' or char =='u':
+#else:
+ #   print("its constant")
+
+  #      ? or
+
+ #   str1 = "aeiouAEIOU"
+#if char in str1:
+ #   print("vowel")
+#else:
+ #   print(("consonnt")
+
+# ! ---> elif ladder using short hand if else
+# Eg:1
+# ? Find greater among 3 variables using short hand if else
+#         a =
+
+
+
+
+
+
+
+
+# ! --------> looping
+
+# looping can be implemented using
+# for loop
+# while loop
+
+# --> for loop
+# ? for loop is used for inteartion, if we know the number of times the loop HAVE TO RUN
+#  ? It is used to iterate the iterables eg(string, list, tuple,  etc....)
+
+# todo ---> syntax in 
+#tor(i=0;i<10;i++){
+#
+#printf("hello");
+# }
+# for syntax in python
+# for userdefined_variable in range(start, end, step): default setp value is 1
+# statement
+# statement
+# statement
+#? Eg:1
+#To  the value from 1 to 10 using for loop
+#for i in range(0, 10): #(n, n-1)
+#    print(i)
+
+# ! Eg 1
+ #to print the value from 1 to 10 using for loop
+
+ # for i in range(0, 10): #(n , n-1)
+ # print("hello")
+
+ # eg-2
+#for val in range (1, 15, 2):
+#  print(val)
+
+# for val in range (1, 15, 3)
+#  print(val)
+
+# for val in range(10, 0, -1):
+# print(val)
+
+#for val in range (10, 0, 1):
+#     print (val)
+
+#Eg:4
+# ! print the output of 7th multiplication tablr from 21 tp 43
+
+#for val in range (21, 43+1, 1):
+#  print(val*7)
+
+
+
+#for val in range(1, 10+1, 1):
+ # print('7','x', val,'=',val*7)
+
+ #method:2
+#ans="7 x {}"
+ # print(ans.format(val, val*7))
+
+ #method:3
+ #print(f"7 x {val}={val*7}")
+
+ #?Eg:5
+# break ---> used to teerminate the loop
+
+
+#for val in range (1, 10):
+ #   if val ==4:
+#        break
+ #   print(val)
+
+ #E:6
+#for val in range(1, 10):
+#    print(val)
+#if val ==5:
+#
+#break
+
+#! continue
+#Eg:1
+#for val in range(1, 100):
+ #   if val ==50:
+#        continue
+ #   print(val)
+
+#for val in range (20, 40):
+ #   if val %2==0:
+ #continue
+#    print(val-1)
+
+# !---------> while loop
+# while is used to when we do not know the number of times the loop have to run
+# ? to oterate the non iterate elements while loop is used
+
+# todo syntex
+# while condition:
+#  stwment
+# increasing to decresasing
+
+
+# Eg:1
+# to itetrate number from 1 to 10
+
+#i = 0
+#while i<11:
+#    print(i)
+
+#for loop practice to display sum of odd numbers and even
+#numbers that fall between
+#12 and 37 (including both numbers)
+
+
+#for val in range (20, 47):
+#    if val %1==0:
+#     continue
+#    print(val-2)
+
+
+    
+        
+ 
+
+ 
+     
+
+
+  
+     
